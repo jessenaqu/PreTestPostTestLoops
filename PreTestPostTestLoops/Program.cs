76f71fd326c6549f0b48a914ad5034ca67c4adc0
Jessena Quinones <JQuinones13@my.gcu.edu>
2020-10-13 00:51:13 +0000

    Commit: In Class Work Example

--- PreTestPostTestLoops/Program.cs
+++ PreTestPostTestLoops/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,41 @@ namespace PreTestPostTestLoops
     {
         static void Main(string[] args)
         {
-            while ();
+            //======================================
+            //          Define Variables 
+            //======================================
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
 
+            //======================================
+            //   Post Test with Do/While Loop
+            //======================================
+            i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 5);
 
+            //======================================
+            //        For Loop (Pre Test)
+            //======================================
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+     
+            for (int x = 0; x <= 10; x = x + 2)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
         }
     }
 }
